c1e4e6b8e8123a8d76c8b654e3007f0c2330654a
Yiyun Yuan <941201yuan@gmail.com>
2014-04-29 09:52:50 +0800

    Small modification

--- AKS.c
+++ AKS.c
@@ -86,9 +86,9 @@ int AKS(fmpz_t n){
 }
 int main(){
     fmpz_t test;fmpz_init(test);
-    FILE *fp;fp=fopen("test.dat","w");
-    for(fmpz_one(test);fmpz_cmp_ui(test,100000)<=0;fmpz_add_ui(test,test,1)){
-        fprintf(fp,"%d\n",AKS(test));
+    while(1){
+        fmpz_read(test);
+        printf("%d\n",AKS(test));
     }
     fmpz_clear(test);
     return 0;
